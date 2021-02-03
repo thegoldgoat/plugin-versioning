4177f48ca9601c06664641ca3e3b0f82fddf78f8
Andrea Somaini <thegoldgoat@gmail.com>
2021-02-03 15:04:35 +0100

    Completed plugin
    1. Exposing 'versionField' field, with default value of 0
    2. Before updating a field, increment the versionField

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+const defaultOptions = {
+  versionField: 'cacheVersion',
+}
+
 export default {
   install(components, installOptions) {
-    console.debug('installing plugin-versioning')
+    const pluginOptions = {
+      ...defaultOptions,
+      ...installOptions,
+    }
+    const versionField = pluginOptions.versionField
+
+    const { Model, Query } = components
+
+    // Expose versionField flag
+    const localFieldModel = {
+      [versionField]: Model.number(0),
+    }
+
+    const _saveGetFiedsMethod = Model.prototype.$fields
+    Model.prototype.$fields = function () {
+      const existing = _saveGetFiedsMethod.call(this)
+      return Object.assign({}, existing, localFieldModel)
+    }
+
+    Query.on('beforeUpdate', function (model) {
+      model[versionField]++
+    })
   },
 }
